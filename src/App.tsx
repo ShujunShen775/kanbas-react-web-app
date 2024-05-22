080a6bb71ade1cc4f375b16e8a1d1f341b280ad0
ShujunShen <shenshujun775@gmail.com>
2024-05-23 04:14:17 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+function App() {
+  return (
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
