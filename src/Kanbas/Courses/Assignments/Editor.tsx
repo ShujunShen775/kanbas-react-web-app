a506f33795b9b69f8316c4b94368fa1d9d378e52
ShujunShen <145154882+ShujunShen775@users.noreply.github.com>
2024-06-06 01:37:50 +0800

    Add files via upload

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,100 +1,164 @@
+import "./index.css";
 export default function AssignmentEditor() {
   return (
     <div id="wd-assignments-editor">
-      <label htmlFor="wd-name">Assignment Name</label>
-      <input id="wd-name" value="A1 - ENV + HTML" />
-      <br />
-      <br />
-      <textarea id="wd-description">
-        The assignment is available online Submit a link to the landing page of
-      </textarea>
-      <br />
-      <table>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-points">Points</label>
-          </td>
-          <td>
-            <input id="wd-points" value={100} />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-assignment-group">Assignment Group</label>
-          </td>
-          <td>
-            <select id="wd-assignment-group">
-              <option value="ASSIGNMENTS">ASSIGNMENTS</option>
-              <option value="MODULES">MODULES</option>
+      <div className="mb-3">
+        <label htmlFor="wd-name" className="form-label">
+          Assignment Name
+        </label>
+        <input
+          id="wd-name"
+          className="form-control mb-3"
+          value="A1 - ENV + HTML"
+        />
+        <textarea
+          id="wd-description"
+          className="form-control col-12"
+          rows={5}
+          style={{ resize: "none" }}
+          value={`The assignment is avallable online Submit a link to the landing page of your Wveb application running onNetlify. The landing page should include the followwing: e Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositorles The Kanbas application should include a link to navigate back to the landing page.`}
+        />
+      </div>
+      <div className="mb-3 row">
+        <label htmlFor="wd-points" className="form-label col-3 text-end">
+          Points
+        </label>
+        <div className="col-9">
+          <input id="wd-points" className="form-control" value="100" />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label htmlFor="wd-group" className="form-label col-3 text-end">
+          Assignment Group
+        </label>
+        <div className="col-9">
+          <select className="form-select" aria-label="Assignment Group">
+            <option value="assignments" selected>
+              ASSIGNMENTS
+            </option>
+            <option value="modules">MODULES</option>
+          </select>
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label htmlFor="wd-grade" className="form-label col-3 text-end">
+          Display Grade as
+        </label>
+        <div className="col-9">
+          <select className="form-select" aria-label="Display Grade as">
+            <option value="percentage" selected>
+              percentage
+            </option>
+            <option value="number">number</option>
+          </select>
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label htmlFor="wd-type" className="form-label col-3 text-end">
+          Submission Type
+        </label>
+        <div className="col-9">
+          <div className="border rounded p-3">
+            <select className="form-select mb-3" aria-label="Submission Type">
+              <option value="Online" selected>
+                Online
+              </option>
+              <option value="modules">MODULES</option>
             </select>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-grade-as">Display Grade as</label>
-          </td>
-          <td>
-            <select id="wd-grade-as">
-              <option value="percentage">percentage</option>
-              <option value="number">number</option>
-            </select>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-submission-type">Submission Type</label>
-          </td>
-          <td>
-            <select id="wd-submission-type">
-              <option value="Online">Online</option>
-              <option value="Modules">Modules</option>
-            </select>
-            <div>Online Entry Options</div>
-            <div>
-              <input type="checkbox" value=""></input>
-              <label htmlFor="flexCheckDefault">Text Entry</label>
+            <label htmlFor="wd-options" className="form-label mb-2">
+              Online Entry Options
+            </label>
+            <div className="mb-3">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                value=""
+              ></input>
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Text Entry
+              </label>
             </div>
-            <div>
-              <input type="checkbox" value=""></input>
-              <label htmlFor="flexCheckDefault">Website URL</label>
+            <div className="mb-3">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                value=""
+              ></input>
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Website URL
+              </label>
             </div>
-            <div>
-              <input type="checkbox" value=""></input>
-              <label htmlFor="flexCheckDefault">Media Recordings</label>
+            <div className="mb-3">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                value=""
+              ></input>
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Media Recordings
+              </label>
             </div>
-            <div>
-              <input type="checkbox" value=""></input>
-              <label htmlFor="flexCheckDefault">Student Annotation</label>
+            <div className="mb-3">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                value=""
+              ></input>
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Student Annotation
+              </label>
             </div>
-            <div>
-              <input type="checkbox" value=""></input>
-              <label htmlFor="flexCheckDefault">File Uploads</label>
+            <div className="mb-3">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                value=""
+              ></input>
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                File Uploads
+              </label>
             </div>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-assign-to">Assign To</label>
-          </td>
-          <td>
-            <input id="wd-assign-to" value="" />
-            <br />
-            <label htmlFor="wd-due">Due</label>
-            <br />
-            <input id="wd-due" value="" type="date" />
-            <td>
-              <label htmlFor="wd-available-from">Available from</label>
-              <br />
-              <input id="wd-available-from" value="" type="date" />
-            </td>
-            <td>
-              <label htmlFor="wd-until">Until</label>
-              <br />
-              <input id="wd-until" value="" type="date" />
-            </td>
-          </td>
-        </tr>
-      </table>
+          </div>
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label htmlFor="wd-assign" className="form-label col-3 text-end">
+          Assign
+        </label>
+        <div className="col-9">
+          <div className="border rounded p-3">
+            <label htmlFor="wd-options" className="form-label mb-3">
+              Assign to
+            </label>
+            <input className="form-control mb-3" value="" />
+            <label htmlFor="wd-options" className="form-label mb-3">
+              Due
+            </label>
+            <input className="form-control mb-3" type="date" value="" />
+            <div className="row">
+              <div className="col-6">
+                <label htmlFor="wd-options" className="form-label">
+                  Available from
+                </label>
+                <input className="form-control" type="date" value="" />
+              </div>
+              <div className="col-6">
+                <label htmlFor="wd-options" className="form-label">
+                  Until
+                </label>
+                <input className="form-control" type="date" value="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="clearfix mb-3">
+        <div className="float-end">
+          <button className="btn btn-secondary me-2">Cancel</button>
+          <button className="btn btn-danger">Save</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -3,33 +3,50 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
-import { Navigate, Route, Routes } from "react-router";
+import Grades from "./Grades";
+import { useLocation, Route, Routes } from "react-router";
+import { FaAlignJustify } from "react-icons/fa";
+import "./index.css";
+
 export default function Courses() {
+  const { pathname } = useLocation();
+  const paths = pathname.split("/");
+  paths.shift();
+  paths.shift();
   return (
     <div id="wd-courses">
-      <h2>Course 1234</h2>
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1 float-start" />
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            {paths.map((i, index) => {
+              if (index === paths.length - 1) {
+                return (
+                  <li className="breadcrumb-item active" aria-current="page">
+                    {i}
+                  </li>
+                );
+              }
+              return <li className="breadcrumb-item">{i}</li>;
+            })}
+          </ol>
+        </nav>
+      </h2>
       <hr />
-      <table>
-        <tr>
-          <td valign="top">
-            <CoursesNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<Home />} />
-              <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<h3>Assignments</h3>} />
-              <Route
-                path="Assignments/:id"
-                element={<h3>Assignment Editor</h3>}
-              />
-              <Route path="Assignments" element={<Assignments />} />
-              <Route path="Assignments/:id" element={<AssignmentEditor />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <CoursesNavigation />
+        </div>
+        <div className="flex-fill main-wrapper">
+          <Routes>
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+            <Route path="Grades" element={<Grades />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
